6720ec876126becacfceb7e47f4cfbeed667d585
Colin Coutts <colintcoutts@gmail.com>
2021-08-27 16:00:07 -0700

    first commit

---
+++ src/views/Profile.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+
+import Highlight from "../components/Highlight";
+import Loading from "../components/Loading";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+
+export const ProfileComponent = () => {
+  const { user } = useAuth0();
+  
+  //user object has the org_id available could grab it here or elsewhere to save it to local storage
+  // real question is where in this sample app would be the right place? Profile doesn't have immediate access as you need to navigate to that page
+  
+
+  return (
+    <Container className="mb-5">
+      <Row className="align-items-center profile-header mb-5 text-center text-md-left">
+        <Col md={2}>
+          <img
+            src={user.picture}
+            alt="Profile"
+            className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
+          />
+        </Col>
+        <Col md>
+          <h2>{user.name}</h2>
+          <p className="lead text-muted">{user.email}</p>
+        </Col>
+      </Row>
+      <Row>
+        <Highlight>{JSON.stringify(user, null, 2)}</Highlight>
+      </Row>
+    </Container>
+  );
+};
+
+export default withAuthenticationRequired(ProfileComponent, {
+  onRedirecting: () => <Loading />,
+});
